8a32477fde40386fcdf02ad3de550a44fb4b1961
test <mohamad.m.darzi@gmail.com>
2018-09-22 23:20:02 +0200

    opdracht4_1

---
+++ pe4_1.py
@@ -0,0 +1,9 @@
+cijferICOR = 8
+cijferPROG = 10
+cijferCSN = 9
+gemiddelde = (cijferCSN + cijferICOR + cijferPROG) / 3
+beloning = 30 * gemiddelde
+overzicht = "Mijn cijfers " + str(gemiddelde) + " leveren een beloning van € " + str(beloning) + " op!"
+print(gemiddelde)
+print(beloning)
+print(overzicht)
